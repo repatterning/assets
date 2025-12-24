2f73644e82a1f4be5d4337628827c9aa899f171b
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-12-24 16:26:50 +0000

    Update config.py

--- config.py
+++ config.py
@@ -16,21 +16,35 @@ class Config:
         excluded for names such as warehouse, storage, depository, etc.<br><br>
         """
 
-        # The project's key name
+        '''
+        Keys
+        '''
+        self.s3_parameters_key = 's3_parameters.yaml'
+        self.argument_key = 'artefacts/architecture/latest/arguments.json'
+
+
+        '''
+        Cloud Prefix: Metadata [vis-à-vis Configurations Bucket]
+        '''
+        self.metadata_ = 'assets/external'
+
+
+        '''
+        Project Metadata
+        '''
+        self.project_tag = 'hydrography'
         self.project_key_name = 'HydrographyProject'
 
-        # Directories
+
+        '''
+        Local Paths
+        '''
         self.warehouse = os.path.join(os.getcwd(), 'warehouse')
         self.pathway_ = os.path.join(self.warehouse, 'assets')
         self.maps_ = os.path.join(self.pathway_, 'maps')
 
-        # Prefix
+
+        '''
+        Cloud Prefix: Destination
+        '''
         self.prefix = self.pathway_.replace(os.getcwd() + os.sep, '')
-
-        # The model assets section
-        self.origin_ = 'assets/latest'
-
-        # Keys, etc
-        self.s3_parameters_key = 's3_parameters.yaml'
-        self.argument_key = 'artefacts/architecture/latest/arguments.json'
-        self.metadata_ = 'distributions/external'
